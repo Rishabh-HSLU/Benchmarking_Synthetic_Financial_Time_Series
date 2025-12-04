aeb36aafc5e558ac3e3388e2766c31927647852d
rishabh <rishabh.yadav@stud.hslu.ch>
2025-12-04 14:52:01 +0100

    regime switching analysis to check the fidelity of synthetic data

--- flowgen.py
+++ flowgen.py
@@ -6,9 +6,12 @@ from ydata.dataset import Dataset
 from ydata.metadata import Metadata
 from ydata.utils.data_types import VariableType
 
-def stock_5y(ticker: str) -> pd.DataFrame:
+from statsmodels.tsa.regime_switching.markov_regression import MarkovRegression
+import plotly.express as px
+
+def stock_5y(ticker: str, period: str = "5y", interval : str= "1d" ) -> pd.DataFrame:
     stock = Ticker(ticker)
-    df = stock.history(period="5y")
+    df = stock.history(period= period, interval = interval)
     df.reset_index(level=0, inplace=True)
     df.drop('symbol', axis= 'columns', inplace=True)
     df = df[['close']].reset_index()
@@ -32,3 +35,48 @@ def synthesis(dataframe: pd.DataFrame) -> pd.DataFrame:
     synth.fit(dataset, metadata=metadata)
 
     return synth.sample(n_entities=1).to_pandas()
+
+def regime_analysis(df: pd.DataFrame, k_regimes = 2) -> list:
+    # create excess price returns dataframe with one day as interval
+    df_returns = pd.DataFrame({'date': df['date'], 'returns': df['close'].diff()})
+    # regime switching using markov regression
+    mod_kns = MarkovRegression(df_returns['returns'].dropna(), k_regimes = k_regimes, trend='n', switching_variance=True)
+    res_kns = mod_kns.fit()
+
+    regime_list = []
+    if k_regimes == 2:
+        low_var = list(res_kns.smoothed_marginal_probabilities[0])
+        high_var = list(res_kns.smoothed_marginal_probabilities[1])
+
+        for i in range(0, len(low_var)):
+            if low_var[i] > high_var[i]:
+                regime_list.append(0)
+            else:
+                regime_list.append(1)
+
+    return regime_list
+
+
+def plot_regime_switching(df: pd.DataFrame, regime_list: list, k_regimes = 2) -> None:
+    df_trim = df.iloc[len(df) - len(regime_list):]
+
+    # build regimes dataframe
+    regimes = (
+        pd.DataFrame({"regimes": regime_list}, index=df_trim.index)
+        .join(df_trim)
+        .reset_index()
+        .rename(columns={"index": "Date"})
+    )
+
+    fig = px.scatter(
+        regimes,
+        x="Date",
+        y="close",
+        color="regimes",
+        color_discrete_map={0: "green", 1: "red", 2: "yellow"},
+        title="Historical SP500 Regimes",
+        opacity=0.8,
+    )
+
+    fig.update_layout(height=700, title_font=dict(size=26))
+    fig.show()
